0c8ed4e04b170fef86551b398153cc8ce4849003
Harrison Newman <harrydnewman@gmail.com>
2025-03-14 21:25:56 -0400

    changed file paths

--- backend/python/pipelines/age_and_race/__init__.py
+++ backend/python/pipelines/age_and_race/__init__.py
@@ -3,9 +3,7 @@ from .face_extraction import extract_faces_from_image
 from .age_classification import age_classification
 from .race_classification import race_classification
 from .helper import get_folder_from_path
-from .helper import move_folder_contents
 from .helper import parse_age_data
-from .helper import write_output_file
 
 import os
 
@@ -18,8 +16,6 @@ def ageandrace(image_path):
             print(path)
             ageTest = age_classification(path)
             ageData = parse_age_data(ageTest)
-
-            image_folder = get_folder_from_path(path)
             race_image_folder = get_folder_from_path(image_path)
             raceClassification = race_classification(race_image_folder)
             data = {
@@ -29,9 +25,6 @@ def ageandrace(image_path):
             }
             os.remove(path)
             return data
-
-            # need to like clean out the age test but let me keep going for now bc this is interesting
-
     else:
         print("No faces were extracted.")
 
