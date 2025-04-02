4e3da4589fd9519146dba7de1332b59cb3417ebc
Harrison Newman <harrydnewman@gmail.com>
2025-04-02 17:09:09 -0400

    getting rid of padding and margins

--- frontend/src/pages/Photo.jsx
+++ frontend/src/pages/Photo.jsx
@@ -1,7 +1,14 @@
+import React from "react";
+import Webcam from "react-webcam";
+import styles from '../styles/Photo.module.css'
 
-
-export default function Photo(){
+export default function Photo() {
     return (
-        <h1>Photo!!</h1>
+        <div className={styles.photoDiv}>
+            {/* <h1>Photo!!</h1> */}
+            <Webcam className={styles.webcam}/>
+        </div>
+
+
     )
 }
