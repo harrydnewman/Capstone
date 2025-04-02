b2b62d4ce67ec61b56fb28d4cd3043ddca138fb8
Harrison Newman <harrydnewman@gmail.com>
2025-04-02 17:20:11 -0400

    chanign camera

--- frontend/src/pages/Photo.jsx
+++ frontend/src/pages/Photo.jsx
@@ -2,13 +2,21 @@ import React from "react";
 import Webcam from "react-webcam";
 import styles from '../styles/Photo.module.css'
 
+const videoConstraints = {
+    width: 500,
+    height: 800,
+    facingMode: "user"
+  };
+
 export default function Photo() {
     return (
         <div className={styles.photoDiv}>
-            {/* <h1>Photo!!</h1> */}
-            <Webcam className={styles.webcam}/>
+            <div className={styles.webcamDiv}>
+                <Webcam className={styles.webcam} height={800} width={500} videoConstraints={videoConstraints}/>
+            </div>
+            <div className={styles.infoDiv}>
+                <h1>Take Photo</h1>
+            </div>
         </div>
-
-
     )
 }
