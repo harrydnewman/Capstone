decb1aec17d5c7965acea65411e4703b0deb42f9
Harrison Newman <harrydnewman@gmail.com>
2025-04-02 19:29:26 -0400

    omg its like giving

--- frontend/src/pages/Test.jsx
+++ frontend/src/pages/Test.jsx
@@ -2,6 +2,7 @@ import React, { useState, useRef, useEffect } from "react";
 // import styles from "..styles/Test.module.css";
 import styles from '../styles/Test.module.css'
 import Webcam from "react-webcam";
+import Spinner from "../components/Spinner";
 
 export default function Test() {
     const [expanded, setExpanded] = useState(false);
@@ -9,18 +10,26 @@ export default function Test() {
     const [image, setImage] = useState(null);
     const [flashActive, setFlashActive] = useState(false);
     const webcamRef = useRef(null);
+    const [loading, setLoading] = useState(null);
+    const [photoTaken, setPhotoTaken] = useState(false);
+
 
     const FLASH_DURATION = 400;
 
-useEffect(() => {
-    if (image) {
-        const timer = setTimeout(() => {
-            setExpanded(prev => !prev);
-            
-        }, FLASH_DURATION);
-        return () => clearTimeout(timer);
-    }
-}, [image]);
+    useEffect(() => {
+        if (image) {
+            const timer = setTimeout(() => {
+                setExpanded(prev => !prev);
+
+
+            }, FLASH_DURATION);
+            return () => clearTimeout(timer);
+        }
+    }, [image]);
+
+    useEffect(() => {
+        setTimeout(() => setLoading(false), 20000);
+    }, [loading]);
 
 
     const handleClick = () => {
@@ -29,6 +38,8 @@ useEffect(() => {
         capture()
         setShowTakeButton(prev => !prev)
         setTimeout(() => setFlashActive(false), FLASH_DURATION);
+        setLoading(true)
+        setPhotoTaken(true)
     };
     const capture = React.useCallback(
         () => {
@@ -45,10 +56,16 @@ useEffect(() => {
                 {flashActive && <div className={styles.flashOverlay}></div>}
                 {/* <button onClick={() => handleClick()}>Toggle</button> */}
                 <div className={styles.webcamDiv}>
-                    <Webcam
+                    {photoTaken ? (
+                        <img src={image} alt="Captured" style={{
+                            width: "60%",
+                            height: "85%",
+                            objectFit: "cover",
+                        }} />
+                    ) : (
+                        <Webcam
                         ref={webcamRef}
                         screenshotFormat="image/png"
-
                         style={{
                             width: "60%",
                             height: "85%",
@@ -56,20 +73,23 @@ useEffect(() => {
                         }}
                         className={styles.webcam}
                     />
-                <div className={`${styles.takeButtonDiv} ${showTakeButton ? styles.takeButtonShown : styles.takeButtonHidden}`}>
-                {/* <div className={styles.takeButtonDiv}> */}
-                    <button className={styles.takeButton} onClick={handleClick}>
-                        <p className={`${showTakeButton ? styles.takeButtonTextShown : styles.takeButtonTextHidden}`}>Take Photo</p>
-                    </button>
-                </div>
-                
+                    )}
+                    
+                    <div className={`${styles.takeButtonDiv} ${showTakeButton ? styles.takeButtonShown : styles.takeButtonHidden}`}>
+                        {/* <div className={styles.takeButtonDiv}> */}
+                        <button className={styles.takeButton} onClick={handleClick}>
+                            <p className={`${showTakeButton ? styles.takeButtonTextShown : styles.takeButtonTextHidden}`}>Take Photo</p>
+                        </button>
+                    </div>
+
                 </div>
             </div>
             <div className={`${styles.box} ${expanded ? styles.rightExpanded : styles.rightCollapsed}`}>
-                <p>This div expands!</p>
-                {image && (
+                {/* <p>This div expands!</p> */}
+                {loading ? <Spinner /> : <p>Loaded!</p>}
+                {/* {image && (
                     <img src={image} alt="Captured" style={{ width: '100%', maxWidth: '300px' }} />
-                )}
+                )} */}
             </div>
         </div>
     );
