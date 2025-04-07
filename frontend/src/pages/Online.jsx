5d4a5f1aa43c07eccbc93577f3710b50e43cf208
Harrison Newman <harrydnewman@gmail.com>
2025-04-07 01:56:11 -0400

    yooo

--- frontend/src/pages/Online.jsx
+++ frontend/src/pages/Online.jsx
@@ -1,5 +1,9 @@
+import styles from '../styles/Online.module.css'
+
 export default function Online(){
     return (
+        <div className={styles.main}>
         <h1>Where you show up online</h1>
+        </div>
     )
 }
