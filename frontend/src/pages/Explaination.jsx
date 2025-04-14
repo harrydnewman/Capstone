170d142116472e3a9d54dc290388d0b01fec2a64
Harrison Newman <harrydnewman@gmail.com>
2025-04-13 22:13:18 -0400

    done for now

--- frontend/src/pages/Explaination.jsx
+++ frontend/src/pages/Explaination.jsx
@@ -4,6 +4,7 @@ export default function Explaination() {
     return (
         <div className={styles.main}>
             <div className={styles.mainPoint}>
+                {/* animate these */}
                 <h1>None of these models are fully reliable.</h1>
                 <h1>Yet, AI systems built on similar data and techniques are used every day in hiring, policing, and immigration screening.</h1>
                 <h1>When these systems fail, the consequences aren&apos;t just wrong answers, they&apos;re life-changing decisions.</h1>
